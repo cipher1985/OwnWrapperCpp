5311c68756ebcce857c8c89546956c6b4d440a95
cipher1985 <fuzhenyong@chinaedu.net>
2025-01-25 16:38:44 +0800

    update commit

--- QtWrapperCpp/QtDelayStateChecker/qtdelaystatechecker.cpp
+++ QtWrapperCpp/QtDelayStateChecker/qtdelaystatechecker.cpp
@@ -1,49 +1,52 @@
 ﻿#include "QtDelayStateChecker.h"
 
 
-QtDelayStateChecker::QtDelayStateChecker() :
-    m_lastUpdateTime(std::chrono::steady_clock::now()) {}
+QtDelayStateChecker::QtDelayStateChecker(QObject* parent) :
+    QObject(parent)
+{
+    m_timer.restart();
+    m_funcPreprocessCheck = [](int value){ return value; };
+}
 
 void QtDelayStateChecker::SetCheckFunc(
-    std::function<int(int)> funcCheck,
-    std::function<void(int)> funcStateChanged)
+    std::function<int(int)> funcPreprocessCheck)
 {
-    m_funcStateCheck = funcCheck;
-    m_funcStateChanged = funcStateChanged;
-    m_lastUpdateTime = std::chrono::steady_clock::now();
+    if(funcPreprocessCheck == nullptr)
+        return;
+    m_funcPreprocessCheck = funcPreprocessCheck;
+    m_timer.restart();
 }
 
 void QtDelayStateChecker::UpdateData(int value)
 {
-    if(!m_funcStateCheck)
-        return;
-    auto currentTime = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
-        (currentTime - m_lastUpdateTime).count();
-    int curState = m_funcStateCheck(value);
+    int curState = m_funcPreprocessCheck(value);
     //检测状态是否一致
     if(curState != m_state) {
-        if(duration > m_delayMS) {
+        if(m_timer.elapsed() > m_delayMS) {
             //超过时长更新状态
             m_state = curState;
-            if(m_funcStateChanged)
-                m_funcStateChanged(m_state);
-            m_lastUpdateTime = std::chrono::steady_clock::now();
+            emit sigStateChanged(m_state);
+            m_timer.restart();
         }
     } else {
         //状态一致更新时间
-        m_lastUpdateTime = std::chrono::steady_clock::now();
+        m_timer.restart();
     }
 }
 
 void QtDelayStateChecker::SetDelayTime(int64_t ms)
 {
     m_delayMS = ms;
-    m_lastUpdateTime = std::chrono::steady_clock::now();
+    m_timer.restart();
 }
 
 void QtDelayStateChecker::SetState(int state)
 {
     m_state = state;
-    m_lastUpdateTime = std::chrono::steady_clock::now();
+    m_timer.restart();
+}
+
+int QtDelayStateChecker::GetState()
+{
+    return m_state;
 }
--- QtWrapperCpp/QtDelayStateChecker/qtdelaystatechecker.h
+++ QtWrapperCpp/QtDelayStateChecker/qtdelaystatechecker.h
@@ -3,20 +3,17 @@
 
 #include <QObject>
 #include <QElapsedTimer>
-#include <chrono>
-#include <thread>
-#include <atomic>
-#include <functional>
+
 //延时转换状态检测器
 class QtDelayStateChecker : public QObject
 {
     Q_OBJECT
 public:
-    QtDelayStateChecker();
-    //设置检测方法
-    void SetCheckFunc(std::function<int(int)> funcCheck,
-        std::function<void(int)> funcStateChanged);
-    //更新数据
+    QtDelayStateChecker(QObject* parent = nullptr);
+    //设置检测方式
+    void SetCheckFunc(
+        std::function<int(int)> funcPreprocessCheck);
+    //更新数据状态值等待延时时长结束则改变当前状态
     void UpdateData(int value);
     //设置检测延时时长
     void SetDelayTime(int64_t ms);
@@ -25,7 +22,7 @@ public:
     //获得当前状态
     int GetState();
 signals:
-    void sigCheck(int );
+    //状态改变通知
     void sigStateChanged(int state);
 private:
     //更新实时数值
@@ -34,14 +31,10 @@ private:
     QAtomicInt m_state{0};
     //延时时长记录
     QElapsedTimer m_timer;
-    //上次更新时间
-    std::chrono::steady_clock::time_point m_lastUpdateTime;
     //更新测算时长
     std::atomic<int64_t> m_delayMS{0};
     //根据数据改变状态规则
-    std::function<int(int)> m_funcStateCheck{};
-    //状态改变回调方法
-    std::function<void(int)> m_funcStateChanged{};
+    std::function<int(int)> m_funcPreprocessCheck{};
 };
 
 #endif // QTDELAYSTATECHECKER_H
